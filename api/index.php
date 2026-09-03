375864f6100d369d241b5e895e316f10638f5322
Makhdum <felisleso@gmail.com>
2026-09-03 10:02:37 +0700

    fix: update api index for laravel 12 vercel serverless storage

--- api/index.php
+++ api/index.php
@@ -1,6 +1,10 @@
 <?php
 
-// Buat folder penyimpanan sementara di /tmp serverless
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Buat direktori penyimpanan sementara di /tmp
 $tmpStorage = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/sessions',
@@ -15,11 +19,21 @@ foreach ($tmpStorage as $dir) {
     }
 }
 
-// Set variabel lingkungan untuk storage & cache Laravel
+// Set variabel lingkungan cache
 putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
 putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
 putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
 putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
 putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
 
-require __DIR__ . '/../public/index.php';
+// Load Autoloader & Bootstrap Laravel 12
+require __DIR__ . '/../vendor/autoload.php';
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+// Paksa seluruh sistem storage Laravel menggunakan /tmp
+$app->useStoragePath('/tmp/storage');
+
+// Jalankan Request
+$request = Request::capture();
+$response = $app->handle($request);
+$response->send();
